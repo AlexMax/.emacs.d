6dfe015ab23317270f1229065b9a1a10b29c83c4
Alex Mayfield <alexmax2742@gmail.com>
2011-11-18 17:38:19 -0500

    Change whitespace characters.

--- my-lisp/my-whitespace.el
+++ my-lisp/my-whitespace.el
@@ -1,4 +1,9 @@
 ; whitespace
 (require 'whitespace)
 
+(setq whitespace-display-mappings
+      '((newline-mark 10 [172 10])
+	(space-mark 32 [183])
+	(tab-mark 9 [182 9])))
+
 (provide 'my-whitespace)
