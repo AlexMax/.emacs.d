7b68eaf43333b2bd81defadb2a71f13079acc85d
Alex Mayfield <alexmax2742@gmail.com>
2011-11-18 17:37:57 -0500

    Enable whitespace in python-mode without the 8-space warning.

--- my-lisp/my-python-mode.el
+++ my-lisp/my-python-mode.el
@@ -1,4 +1,8 @@
 ; python-mode
 (require 'python-mode)
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (setq whitespace-style (delq 'indentation whitespace-style))
+	    (whitespace-mode t)))
 
 (provide 'my-python-mode)
