4e5c26305731477a9cf9efc25ab82a09af0cfe8a
Alex Mayfield <alexmax2742@gmail.com>
2011-12-01 11:58:12 -0500

    Change default c-style to Linux.

--- my-lisp/my-settings.el
+++ my-lisp/my-settings.el
@@ -1,6 +1,7 @@
 ; Standard options
 (setq auto-save-default nil)
 (setq backup-inhibited t)
+(setq c-default-style "linux")
 (setq inhibit-splash-screen t)
 (setq linum-format "%d ")
 (setq show-trailing-whitespace t)
