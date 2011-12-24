af1ca557a73c27b4e195d5512dae74921387e188
AlexMax <alexmax2742@gmail.com>
2011-12-24 13:23:46 -0500

    Add cmake-mode.

--- my-lisp/my-settings.el
+++ my-lisp/my-settings.el
@@ -6,7 +6,7 @@
 (setq linum-format "%d ")
 (setq show-trailing-whitespace t)
 (setq-default truncate-lines t)
-(menu-bar-mode nil)
-(tool-bar-mode nil)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
 
 (provide 'my-settings)
